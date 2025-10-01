82f74d81bb2584597a92f4e9bb6c6def4c006c8e
ict-206-pc-28-sme0215 <MatimotTheTimoters@gmail.com>
2025-10-01 12:25:41 +0800

    chore: init FornNewLoan layout and design

--- LoanCalculator/FormNewLoan.Designer.cs
+++ LoanCalculator/FormNewLoan.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormNewLoan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormNewLoan";
+            this.ClientSize = new System.Drawing.Size(480, 757);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "FormNewLoan";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "New Loan";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
