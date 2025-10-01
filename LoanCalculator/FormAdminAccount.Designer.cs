09601103aa0d6a40a9e398bf2f7e343339604686
ict-206-pc-28-sme0215 <MatimotTheTimoters@gmail.com>
2025-10-01 11:34:10 +0800

    style: set sizing properties on all MDI container form files

--- LoanCalculator/FormAdminAccount.Designer.cs
+++ LoanCalculator/FormAdminAccount.Designer.cs
@@ -34,10 +34,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(484, 761);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.IsMdiContainer = true;
             this.Name = "FormAdminAccount";
-            this.Text = "FormAdminAccount";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Admin Account";
             this.ResumeLayout(false);
 
         }
--- LoanCalculator/FormNewUser.Designer.cs
+++ LoanCalculator/FormNewUser.Designer.cs
@@ -28,21 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStripNewUser = new System.Windows.Forms.MenuStrip();
             this.SuspendLayout();
+            // 
+            // menuStripNewUser
+            // 
+            this.menuStripNewUser.Location = new System.Drawing.Point(0, 0);
+            this.menuStripNewUser.Name = "menuStripNewUser";
+            this.menuStripNewUser.Size = new System.Drawing.Size(484, 24);
+            this.menuStripNewUser.TabIndex = 1;
+            this.menuStripNewUser.Text = "menuStrip1";
             // 
             // FormNewUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(484, 761);
+            this.Controls.Add(this.menuStripNewUser);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStripNewUser;
             this.Name = "FormNewUser";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "New User";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStripNewUser;
     }
 }
 
