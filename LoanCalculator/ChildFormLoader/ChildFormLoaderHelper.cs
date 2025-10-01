132bf4a99a124a16a63942873eb7c3dcd8b4b3d6
ict-206-pc-28-sme0215 <MatimotTheTimoters@gmail.com>
2025-10-01 11:39:59 +0800

    feat: create ChildFoamLoaderHelper
    What:
    - Contains LoadChildForm method

---
+++ LoanCalculator/ChildFormLoader/ChildFormLoaderHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoanCalculator.ChildFormLoader
+{
+    class ChildFormLoaderHelper
+    {
+        public static void LoadChildForm(Form childForm, Form parentForm)
+        {
+            foreach (Form currentForm in parentForm.MdiChildren)
+            {
+                if (currentForm.GetType() == parentForm.GetType())
+                {
+                    currentForm.Focus();
+                    return;
+                }
+            }
+
+            childForm.MdiParent = parentForm;
+            childForm.StartPosition = FormStartPosition.CenterScreen;
+            childForm.Show();
+        }
+    }
+}
