20103d6e7dd57b5320a2b7d6e76b5d5a4a2ae690
ict-206-pc-28-sme0215 <MatimotTheTimoters@gmail.com>
2025-10-01 11:28:30 +0800

    chore: create all necessary Form files

--- LoanCalculator/FormNewUser.cs
+++ LoanCalculator/FormNewUser.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace LoanCalculator
 {
-    public partial class Form1: Form
+    public partial class FormNewUser: Form
     {
-        public Form1()
+        public FormNewUser()
         {
             InitializeComponent();
         }
