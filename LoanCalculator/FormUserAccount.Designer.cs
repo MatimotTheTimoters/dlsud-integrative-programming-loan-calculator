ba07422170c232dd6dc8ebf419e1622766cf6e82
ict-206-pc-28-sme0215 <MatimotTheTimoters@gmail.com>
2025-10-01 11:29:30 +0800

    feat: set NewUser, UserAccount and AdminAccount as MDI Containers

--- LoanCalculator/FormUserAccount.Designer.cs
+++ LoanCalculator/FormUserAccount.Designer.cs
@@ -35,6 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.IsMdiContainer = true;
             this.Name = "formUserAccount";
             this.Text = "FormUserAccount";
             this.ResumeLayout(false);
